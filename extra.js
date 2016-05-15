5bc97d3f15f31f98313ff3976cadd33efcd832fb
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-05-15 20:10:03 -0300

    Typo

--- extra.js
+++ extra.js
@@ -2,7 +2,7 @@ var console = {
   lines: [],
   log: function(line) {
     this.lines.push(line + "\n");
-  }
+  },
   toString: function() {
     return String.prototype.concat.apply([], this.lines);
   }
