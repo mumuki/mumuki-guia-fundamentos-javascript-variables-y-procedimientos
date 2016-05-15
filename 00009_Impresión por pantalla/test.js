96d9e93994d4c558f029f8fce1f7b967fd2e8dde
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-05-15 20:12:01 -0300

    Typo

--- 00009_Impresión por pantalla/test.js
+++ 00009_Impresión por pantalla/test.js
@@ -1,7 +1,7 @@
 describe("versos", function() {
   it("deberia imprimir los versos", function() {
     versosMartinFierro()
-    assert.equal(console.toString(), "Aquí me pongo a cantar\nAl compás de la vigüela;\n Que el hombre que lo desvela\nUna pena extraordinaria")
+    assert.equal(console.toString(), "Aquí me pongo a cantar\nAl compás de la vigüela;\nQue el hombre que lo desvela\nUna pena extraordinaria\n")
   });
   it("deberia retornar 0", function() {
     assert.equal(versosMartinFierro(), 0);
