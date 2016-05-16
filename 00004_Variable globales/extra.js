2819852edc4a559c6f84ab38ae0fc3b6406f90d2
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-05-16 17:28:38 -0300

    Corrigiendo ejercicio 4

---
+++ 00004_Variable globales/extra.js
@@ -0,0 +1,1 @@
+var pesoPromedioPersonaEnKilogramos = 75;
--- 00004_Variable globales/test.js
+++ 00004_Variable globales/test.js
@@ -1,18 +1,18 @@
 describe("asensorSobrecagado", function() {
   it("asensorSobrecagado(4) con pesoPromedioPersonaEnKilogramos = 70", function() {
-    var pesoPromedioPersonaEnKilogramos = 70;
+    pesoPromedioPersonaEnKilogramos = 70;
     assert(!asensorSobrecagado(4))
   })
   it("asensorSobrecagado(4) con pesoPromedioPersonaEnKilogramos = 80", function() {
-    var pesoPromedioPersonaEnKilogramos = 80;
+    pesoPromedioPersonaEnKilogramos = 80;
     assert(asensorSobrecagado(4))
   })
   it("asensorSobrecagado(2) con pesoPromedioPersonaEnKilogramos = 80", function() {
-    var pesoPromedioPersonaEnKilogramos = 80;
+    pesoPromedioPersonaEnKilogramos = 80;
     assert(!asensorSobrecagado(2))
   })
   it("asensorSobrecagado(5) con pesoPromedioPersonaEnKilogramos = 80", function() {
-    var pesoPromedioPersonaEnKilogramos = 80;
+    pesoPromedioPersonaEnKilogramos = 80;
     assert(asensorSobrecagado(5))
   })
 })
